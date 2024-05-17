3b305f289eb9915e7efd0b54bb012bde695fa46b
Raui <rauivm@hotmail.com>
2024-05-17 17:21:33 -0300

    Add css template
    Correção dos testes unitários

--- test_soma.py
+++ test_soma.py
@@ -15,10 +15,11 @@ class SomaTest(unittest.TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertIn(b'Resultado: -6', response.data)
 
-#    def test_soma_com_valor_nao_numerico(self):
-#        with app.test_client() as test_client:
-#            response = test_client.post('/', data=dict(num1='a', num2=3))
-#            self.assertEqual(response.status_code, 400)
+    def test_soma_com_valor_nao_numerico(self):
+        with app.test_client() as test_client:
+            response = test_client.post('/', data=dict(num1='a', num2=3))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Por favor, insira apenas numeros.', response.data)
 
 if __name__ == '__main__':
     unittest.main()
